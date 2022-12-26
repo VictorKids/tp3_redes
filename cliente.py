6d58d8be9d83f0d6daf8cf2ddb98391126588538
Victor <48036482+VictorKids@users.noreply.github.com>
2022-12-26 13:34:07 -0300

    less bytes for each msg

--- cliente.py
+++ cliente.py
@@ -6,37 +6,28 @@ import selectors
 # UTILITIES
 # #########################################################
 
-def to_2bytes(_inte):
-    inte = bin(_inte)
-    inte = inte[2:]
-    while len(inte) < 16:
-        inte = '0' + inte
-    return inte
-
-def to_4bytes(_inte):
-    inte = bin(_inte)
-    inte = inte[2:]
-    while len(inte) < 32:
+def to_size(inte, size):
+    while len(inte) < size:
         inte = '0' + inte
     return inte
 
 def make_header(t, did):
     global msg_count
     if   t == 1:
-        type = "0000000000000001" 
+        type = "01" 
     elif t == 2:
-        type = "0000000000000010"
+        type = "02"
     elif t == 3:
-        type = "0000000000000011"
+        type = "03"
     elif t == 4:
-        type = "0000000000000100"
+        type = "04"
     else:
-        type = "0000000000000101"
-    return type + id + to_2bytes(int(did)) + to_2bytes(msg_count)
+        type = "05"
+    return type + id + to_size(did, 5) + to_size(str(msg_count), 5)
 
 def send_OK(did, sock, numseq):
     global id
-    str_msg = "0000000000000001" + id + did + numseq + "OK"
+    str_msg = "01" + id + did + numseq + "OK"
     sent = sock.send(str_msg.encode())
 
 # #########################################################
@@ -51,24 +42,24 @@ def read_input(sock):
         #flag  = in_str[0]
         d_id   = in_str[1]
         msg    = in_str[2] 
-        ready_msg = make_header(5, d_id) + to_4bytes(len(msg)) + msg
+        ready_msg = make_header(5, d_id) + to_size(str(len(msg)), 4) + msg
         sent   = sock.send(ready_msg.encode())
         while True:
             ack = sock.recv(1024)
             ack = ack.decode()
-            if ack[0:16] == "0000000000000001":
+            if ack[0:2] == "01":
                 break
             else:
                 print("[ERROR] Servidor mandou uma msg que não é um ACK.")
         msg_count += 1
     elif in_str[0] == "S":
         global sel
-        end_msg = make_header(4, "1111111111111111") + "END"
+        end_msg = make_header(4, "65535") + "END"
         sent    = sock.send(end_msg.encode())
         while True:
             ack = sock.recv(1024)
             ack = ack.decode()
-            if ack[0:16] == "0000000000000001":
+            if ack[0:2] == "01":
                 break
             else:
                 print("[ERROR] Servidor mandou uma msg que não é um ACK.")
@@ -87,17 +78,17 @@ def read_server(sock):
     data = sock.recv(1024)
     if data:
         data = data.decode()
-        msg_type  = data[0:16]
-        origin_id = data[16:32]
-        destin_id = data[32:48]
-        seq_num   = data[48:64]
-        _message   = data[64:]
-        if msg_type == "0000000000000101": # MSG
-            msg_tam = _message[0:32]
-            message = _message[32:]
+        msg_type  = data[0:2]
+        origin_id = data[2:7]
+        destin_id = data[7:12]
+        seq_num   = data[12:17]
+        _message   = data[17:]
+        if msg_type == "05": # MSG
+            msg_tam = _message[0:4]
+            message = _message[4:]
             print(f"[RECV] Mensagem de {origin_id}: {message}.")
-            send_OK("1111111111111111", sock, seq_num)
-        elif msg_type == "0000000000000010": # ERRO
+            send_OK("65535", sock, seq_num)
+        elif msg_type == "02": # ERRO
             print("[ERROR] Servidor reportou um erro.")
         else:
             print("[ERROR] Servidor mandou uma mensagem de tipo indefinido.")
@@ -111,19 +102,19 @@ def read_server(sock):
 sel = selectors.DefaultSelector()
 msg_count = 0
 
-id        = to_2bytes(int(sys.argv[1]))
+id        = to_size(str(sys.argv[1]), 5)
 serv_ip   = sys.argv[2]
 serv_port = int(sys.argv[3])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect_ex((serv_ip,serv_port))
 print("[SYNC] Achei o servidor.")
-hello_msg = make_header(3, "1111111111111111") + "oi"
+hello_msg = make_header(3, "65535") + "oi"
 _ = sock.send(hello_msg.encode())
 while True:
     _ack = sock.recv(1024)
     _ack = _ack.decode()
-    if _ack[0:16] == "0000000000000001":
+    if _ack[0:2] == "01":
         print("[CONN] Conexão com o servidor estabelecida.")
         break
     else:
--- servidor.py
+++ servidor.py
@@ -7,7 +7,7 @@ import selectors
 # #########################################################
 
 sel = selectors.DefaultSelector()
-ID   = "1111111111111111"
+ID   = "65535"
 class Clients:
     def __init__(self, id, socket, info):
         self.id = id
@@ -33,13 +33,13 @@ def msg_read(client):
     data = client.recv(1024)
     if data:
         data = data.decode()
-        msg_type  = data[0:16]
-        origin_id = data[16:32]
-        destin_id = data[32:48]
-        seq_num   = data[48:64]
-        message   = data[64:]
+        msg_type  = data[0:2]
+        origin_id = data[2:7]
+        destin_id = data[7:12]
+        seq_num   = data[12:17]
+        message   = data[17:]
 
-        if msg_type == "0000000000000011": # OI
+        if msg_type == "03": # OI
             print(f"[CONN] mensagem do tipo OI recebida de {origin_id}.")
             new_id_flag = True
             for c in clients:
@@ -59,11 +59,11 @@ def msg_read(client):
                     security_check = True
                     break
             if security_check == True:
-                if   msg_type == "0000000000000010": # ERRO
+                if   msg_type == "02": # ERRO
                     print(f"[ERROR] cliente {origin_id} não deveria mandar msgs do tipo ERRO ao servidor.")
-                elif msg_type == "0000000000000001": # OK 
+                elif msg_type == "01": # OK 
                     print(f"[ACK] confirmação recebida de {origin_id}.")
-                elif msg_type == "0000000000000100": # FLW
+                elif msg_type == "04": # FLW
                     print(f"[DCONN] desconectando cliente {origin_id}.")
                     send_OK(origin_id, client, seq_num)
                     for c in clients:
@@ -72,9 +72,9 @@ def msg_read(client):
                             clients.remove(c)
                             client.close()
                             break
-                elif msg_type == "0000000000000101": # MSG
+                elif msg_type == "05": # MSG
                     print(f"[MSG] mensagem recebida de {origin_id} para {destin_id}.")
-                    if destin_id == "0000000000000000":
+                    if destin_id == "00000":
                         send_broadcast(origin_id, destin_id, message, client, seq_num)
                     else:
                         destin_flag = False
@@ -100,20 +100,21 @@ def msg_read(client):
 # #########################################################
 
 def send_OK(id, cli, numseq):
-    str_msg = "0000000000000001" + ID + id + numseq + "OK"
+    str_msg = "01" + ID + id + numseq + "OK"
     sent = cli.send(str_msg.encode())
 
 def send_ERRO(id, cli, numseq):
-    str_msg = "0000000000000010" + ID + id + numseq + "ERRO"
+    str_msg = "02" + ID + id + numseq + "ERRO"
     sent = cli.send(str_msg.encode())
 
 def send_unicast(oid, did, msg, d_cli, numseq):
-    str_msg = "0000000000000101" + oid + did + numseq + msg
+    str_msg = "05" + oid + did + numseq + msg
     sent = d_cli.socket.send(str_msg.encode())
     while(True):
         ack  = d_cli.socket.recv(1024)
         ack  = ack.decode()
-        if ack[0:16] == "0000000000000001":
+        if ack[0:2] == "01":
+            print(f"[OK] Confirmação recebida de {oid}.")
             break
         else:
             print(f"[ERROR] {d_cli.id} mandou uma msg que não é um ACK.")
